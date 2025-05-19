4fae02538fae342ae4402db4ae672a4e77764273
Mohamed Kamel Dhakwani <medkamel.dhakwani@gmail.com>
2025-05-19 14:49:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/app/(publicClient)
    > You cannot have two parallel pages that resolve to the same path.
    >

--- src/app/(app)/client/[hostId]/[refId]/page.tsx
+++ src/app/(app)/client/[hostId]/[refId]/page.tsx
@@ -1,3 +1,4 @@
+
 // This page is deprecated and has been moved to the (publicClient) route group.
 // This placeholder is to prevent routing conflicts.
 // Please delete this file (/src/app/(app)/client/[hostId]/[refId]/page.tsx) if no longer needed.
